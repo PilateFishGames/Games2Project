da630c8fb3843675d5e9f5b76e0f0accc05cf318
Naomi Foley <foley_naomi@columbusstate.edu>
2016-02-18 13:20:17 -0500

    Prototype
    this is the current in progress prototype

---
+++ Prototype/Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	private float health = 100.0f;
+
+	private float damage = 10.0f;
+
+	public float speed = 5.0f;
+
+	private PlayerController player;
+	private Vector3 playerPosition;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerPosition = player.transform.position;
+		Vector3 direction = playerPosition - transform.position;
+		float distance = direction.magnitude;
+		direction = direction.normalized;
+		float move = speed * Time.deltaTime;
+		if (move > distance) move = distance;
+		transform.Translate (direction * move);
+	}
+
+	//void OnCollisionEnter(Collision other) {
+	//	other.gameObject.GetComponent<Shooting> ().dealDamage(damage);
+	//}
+
+	public void dealDamage(float damage) {
+		health -= damage;
+		if (health <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
